5667df94a116d67a89e0a286fcb3feb0a47f30cc
wangjie <wangjie@taou.com>
2019-10-11 23:06:19 +0800

    优化输出

--- py_str_to_json.py
+++ py_str_to_json.py
@@ -59,5 +59,10 @@ print '''
 str_json = "{aaa:123,bbb:{ccc:456,ddd:{eee:789}},ggg:999,hhh:[{lll:123,kkk:666}]}"
 str_json_1 = '{code: 0, data: {edu: [{school: "北京林业大学", school_url: "http://www.taobao.com", school_url_1: "https://www.baidu.com", description: "A hundred miles", sid: 0, sdegree: "1", v: "2006-01-01", department: "统计学"}], name: "qa123456", exp: [{company_info: { name: "alibaba", cid: 2938}, description: "be a hero", company: "alibaba", worktime: "10", v: "2018-07-01", position: ">测试工程师"}], mem_st: 0, judge: 0, position: "测试工程师", company: "alibaba", mem_id: 0, mmid: "mmid"}, last_url: "https://www.163.com"}'
 
+print "=======================【case_01】start test ========================="
 print json.loads(str_to_json_final(str_json))
+print "=======================【case_01】test  done ========================="
+
+print "=======================【case_02】start test ========================="
 print json.loads(str_to_json_final(str_json_1))
+print "=======================【case_02】test  done ========================="
